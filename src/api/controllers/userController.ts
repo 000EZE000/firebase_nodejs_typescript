94df5cd76802beb4d5b89e5c28d9368f1883f32c
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-26 15:34:26 -0300

    <REFACTOR><MODULE> move endpoint signUp to authController

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -5,18 +5,6 @@ import { STATUS } from "../util/statusResponse";
 
 const router = Router();
 
-router.post("/signUp", async ({ body }: Request, res: Response) => {
-  try {
-    const { failed, content } = await UserService.createUser(body);
-
-    const returnStatus = failed ? STATUS.BAD_REQUEST : STATUS.CREATE;
-
-    return res.status(returnStatus).json({ content });
-  } catch (error: any) {
-    return res.status(STATUS.ERROR_SERVER).json({ content: error.message });
-  }
-});
-
 router.put("/update", async ({ body }: Request, req: Response) => {
   try {
     const responseSevice = await UserService.updateUser(body);
