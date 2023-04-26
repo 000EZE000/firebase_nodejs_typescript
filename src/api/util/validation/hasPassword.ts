7b6679e0f524c9af63fd07bc4f584f1c370309d4
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-26 17:09:22 -0300

    <FIX><MOVE> HashPassword class should not be in firebase folder

--- src/api/util/validation/hasPassword.ts
+++ src/api/util/validation/hasPassword.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import UserEntity from "../../../../core/userEntity";
+import UserEntity from "../../../core/userEntity";
 
 export default class HashPassword {
   static hashPassword = async (user: UserEntity) => {
