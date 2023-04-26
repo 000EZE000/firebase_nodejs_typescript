49a03585a1dff9776d05321085d14a857373dcda
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-25 22:37:39 -0300

    <ADD><ROUTER> router User and middeware parser Json

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import router from "./api/controllers/userController";
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+
+app.use("/user", router);
 
 app.use("/", (_req, res) => {
   res.status(200).json({ message: "hello word" });
