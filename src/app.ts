9b4adcbda7dc0aaf0cea8e03dd6c596aa2f117ff
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-26 15:40:53 -0300

    <REFACTOT><MODULE>rename import and add endpoint autController

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import router from "./api/controllers/userController";
+import routerUser from "./api/controllers/userController";
+import routerAuth from "./api/controllers/authController";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-app.use("/user", router);
+app.use("/user", routerUser);
+app.use("/auth", routerAuth);
 
 app.use("/", (_req, res) => {
   res.status(200).json({ message: "hello word" });
