48aa860aaaa5063241a5215997dd1f09c3bb9eee
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-27 11:09:32 -0300

    <ADD> endpoint authController

--- src/api/controllers/authController.ts
+++ src/api/controllers/authController.ts
@@ -1,13 +1,12 @@
 import { Router, Request, Response } from "express";
-
-import UserService from "../services/UserServices";
 import { STATUS } from "../util/statusResponse";
+import AuthService from "../services/AuthService";
 
 const router = Router();
 
 router.post("/signUp", async ({ body }: Request, res: Response) => {
   try {
-    const { failed, content } = await UserService.createUser(body);
+    const { failed, content } = await AuthService.signUp(body);
 
     const returnStatus = failed ? STATUS.BAD_REQUEST : STATUS.CREATE;
 
@@ -15,6 +14,18 @@ router.post("/signUp", async ({ body }: Request, res: Response) => {
   } catch (error: any) {
     return res.status(STATUS.ERROR_SERVER).json({ content: error.message });
   }
+});
+
+router.post("/signIn", async ({ body }: Request, res: Response) => {
+  const { content, failed } = await AuthService.signIn(body);
+
+  if (failed || typeof content === "string")
+    return res.status(STATUS.BAD_REQUEST).json({ content });
+
+  return res
+    .status(STATUS.OK)
+    .header("token", content.token)
+    .json({ content: content.user });
 });
 
 export default router;
