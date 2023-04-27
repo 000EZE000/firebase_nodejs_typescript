fd758776086c827a41a6dca4d7c3b37ab6f489e9
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-27 11:15:11 -0300

    <ADD> environment JWT_SECRET

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,13 +1,14 @@
 import "dotenv/config";
 
-export const PORT = Number(process.env.PORT ?? 3000);
-export const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY ?? "";
-export const FIREBASE_AUTH_DOMAIN = process.env.FIREBASE_AUTH_DOMAIN ?? "";
-export const FIREBASE_PROYECT_ID = process.env.FIREBASE_PROYECT_ID ?? "";
-export const FIREBASE_STORAGE_BUCKET =
-  process.env.FIREBASE_STORAGE_BUCKET ?? "";
-export const FIREBASE_MESSAGING_SENDER_ID =
-  process.env.FIREBASE_MESSAGING_SENDER_ID ?? "";
-export const FIREBASE_APP_ID = process.env.FIREBASE_APP_ID ?? "";
-export const FIREBASE_MEASUREMENT_ID =
-  process.env.FIREBASE_MEASUREMENT_ID ?? "";
+export const {
+  PORT,
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROYECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} = process.env;
+
+export const JWT_SECRET = process.env.JWT_SECRET ?? "";
