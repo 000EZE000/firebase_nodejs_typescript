5e9400e4ebaa521cce2c510e7e525517c691601e
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-27 21:47:22 -0300

    <ADD> middleware signIn and signUp

--- src/api/controllers/authController.ts
+++ src/api/controllers/authController.ts
@@ -1,22 +1,21 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 import { STATUS } from "../util/statusResponse";
 import AuthService from "../services/AuthService";
+import { signInMiddlware, singUpMiddleware } from "../middlewares/middlewareAuth";
 
 const router = Router();
 
-router.post("/signUp", async ({ body }: Request, res: Response) => {
+router.post("/signUp", singUpMiddleware, async ({ body }, res) => {
   try {
     const { failed, content } = await AuthService.signUp(body);
-
     const returnStatus = failed ? STATUS.BAD_REQUEST : STATUS.CREATE;
-
     return res.status(returnStatus).json({ content });
   } catch (error: any) {
     return res.status(STATUS.ERROR_SERVER).json({ content: error.message });
   }
 });
 
-router.post("/signIn", async ({ body }: Request, res: Response) => {
+router.post("/signIn", signInMiddlware, async ({ body }, res) => {
   const { content, failed } = await AuthService.signIn(body);
 
   if (failed || typeof content === "string")
