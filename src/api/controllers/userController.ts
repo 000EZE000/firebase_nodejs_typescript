4394c54b4366b6e230b55700b516355af2053166
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-29 00:46:53 -0300

    <ADD><FORMAT> middlewares middlewareCheckToken and middlewareCheckTokenAndUse

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -1,28 +1,41 @@
-import { Router, Request, Response } from "express";
-
+import { Router } from "express";
 import UserService from "../services/UserServices";
 import { STATUS } from "../util/statusResponse";
+import {
+  middlewareCheckToken,
+  middlewareCheckTokenAndUser,
+} from "../middlewares/middlewareUser";
+import authRequest from "../global/typeExpress";
 
 const router = Router();
-
-router.put("/update", async ({ body }: Request, req: Response) => {
+router.put("/update", middlewareCheckTokenAndUser, async (req, res) => {
   try {
+    const body = { ...req.body, id: (req as authRequest).userId };
     const responseSevice = await UserService.updateUser(body);
-    return req.status(STATUS.OK).json(responseSevice);
+    return res.status(STATUS.OK).json(responseSevice);
   } catch (error: any) {
-    return req.status(STATUS.ERROR_SERVER).json({ content: error.message });
+    return res.status(STATUS.ERROR_SERVER).json({ content: error.message });
   }
 });
 
-router.delete(
-  "/delete/:id",
-  async ({ params: { id } }: Request, res: Response) => {
-    try {
-      const responseService = await UserService.deleteUser(id);
-      return res.status(STATUS.OK).json(responseService);
-    } catch (error: any) {
-      return res.status(STATUS.ERROR_SERVER).json({ content: error.message });
-    }
-  },
-);
+router.get("/profile", middlewareCheckToken, async (req, res) => {
+  try {
+    const { userId } = req as authRequest;
+    const responseSevice = await UserService.getUser(userId);
+    return res.status(STATUS.OK).json(responseSevice);
+  } catch (error: any) {
+    return res.status(STATUS.ERROR_SERVER).json({ content: error.message });
+  }
+});
+
+router.delete("/delete", middlewareCheckToken, async (req, res) => {
+  try {
+    const { userId } = req as authRequest;
+    const responseService = await UserService.deleteUser(userId);
+    return res.status(STATUS.OK).json(responseService);
+  } catch (error: any) {
+    return res.status(STATUS.ERROR_SERVER).json({ content: error.message });
+  }
+});
+
 export default router;
