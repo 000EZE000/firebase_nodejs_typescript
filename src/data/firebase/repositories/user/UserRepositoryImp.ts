8739fb972631f9626d2701a6e1b9c330a17ca373
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-29 00:32:23 -0300

    <FORMAT><REFACTOR> delete space and console.log

--- src/data/firebase/repositories/user/UserRepositoryImp.ts
+++ src/data/firebase/repositories/user/UserRepositoryImp.ts
@@ -1,4 +1,10 @@
-import { deleteDoc, getDocs, setDoc, updateDoc } from "firebase/firestore";
+import {
+  deleteDoc,
+  getDoc,
+  getDocs,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 import UserRepository from "./UserReposity";
 import UserModel from "../../models/userModel";
 import UserEntity, {
@@ -9,7 +15,6 @@ export default class UserRepositoyIpm implements UserRepository {
   createUser = async (user: UserEntity) => {
     try {
       await setDoc(UserModel.run(user.id), user);
-
       return {
         content: "the user has been create successfully",
       };
@@ -34,24 +39,27 @@ export default class UserRepositoyIpm implements UserRepository {
   findByEmail = async (email: UserEntity["email"]) => {
     try {
       const getUsers = await getDocs(UserModel.find(email));
-
       if (getUsers.empty) throw new Error();
-
       const user = getUsers.docs[0].data() as UserEntity;
-
       return { content: user };
     } catch (error: any) {
-      console.log(error.message);
+      return { content: null };
+    }
+  };
 
+  findById = async (id: UserEntity["id"]) => {
+    try {
+      const getUser = (await getDoc(UserModel.run(id))).data() as UserEntity;
+      if (!getUser) return { content: null };
+      return { content: getUser };
+    } catch (error: any) {
       return { content: null };
     }
   };
 
   deleteUser = async (id: UserEntity["id"]) => {
     try {
-      const actionOfDelete = await deleteDoc(UserModel.run(id));
-      console.log(actionOfDelete);
-
+      await deleteDoc(UserModel.run(id));
       return { content: "the user has been deleted successfully" };
     } catch (error: any) {
       return { content: null };
