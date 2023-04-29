b6e5930e84d4f8192e0d60b701258d940228792f
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-29 00:38:40 -0300

    <DELETE> log to debug middleware

--- src/api/middlewares/middlewareAuth.ts
+++ src/api/middlewares/middlewareAuth.ts
@@ -1,7 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 import {
-  DEBUG_SIGN_IN,
-  DEBUG_SIGN_UP,
   isBodyIncorrectSignIn,
   isBodyIncorrectSignUp,
 } from "../util/validation/checkType";
@@ -12,14 +10,12 @@ type typeMiddleware = (req: Request, res: Response, next: NextFunction) => void;
 const messageError = { content: "the information sent is incorrect" };
 
 export const singUpMiddleware: typeMiddleware = ({ body }, res, next) => {
-  DEBUG_SIGN_UP(body);
   if (isBodyIncorrectSignUp(body))
     return res.status(STATUS.BAD_REQUEST).json(messageError);
   next();
 };
 
 export const signInMiddlware: typeMiddleware = ({ body }, res, next) => {
-  DEBUG_SIGN_IN(body);
   if (isBodyIncorrectSignIn(body))
     return res.status(STATUS.BAD_REQUEST).json(messageError);
   next();
